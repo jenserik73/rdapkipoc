55bc3af7f2981192398eb03fe78aa2386f7c9902
Jens Erik Myhra <jens_erik_myhra@hotmail.com>
2026-03-13 17:36:46 +0100

    OCI_GEN_AI_PROFILE first commit

---
+++ oci_gen_ai_profile.sql
@@ -0,0 +1,11 @@
+select distinct AAR
+from bemanning;
+
+begin
+    DBMS_CLOUD_AI.set_profile(profile_name => 'OCI_GEN_AI_PROFILE');
+end;
+/
+
+select ai showsql lag en oversikt over totalt antall årsverk per år for leger i perioden 2015 til 2025;
+
+select ai narrate predict the total overtime hours for doctors in 2027 based on the trend from 2015 to 2025;
